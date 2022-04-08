6d2395b114c540521e878b2b9ccd5234da6a3aeb
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-04-08 21:50:54 +0530

    Addons

--- index.js
+++ index.js
@@ -27,3 +27,43 @@ promiseObj
 });
 
 
+// Asynchronus JS
+/*
+There are two main types of asynchronus code style
+you will come across in JavaScript.
+
+1. Callback based:
+    eg:
+        1. setInterval(), 2. setTimeout()
+2. Promise based:
+    eg:
+        1. fetch()        
+*/
+
+// Creating your own promise object
+/*
+ 
+const myPromise = new Promise((resolFunc, rejectFunc) => {
+    asyncTask;
+}); 
+
+resolFunc ====> called on resolve of promise.
+rejectFunc ===> called on reject of promise.
+ */
+
+const myPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // resolve("1 second completed");
+            reject("Rejected");
+        }, 1000);
+    });
+}
+
+myPromise().then((fromResolve) => {
+    console.log(fromResolve);
+})
+.catch((fromReject) => {
+    console.log(fromReject);
+});
+
